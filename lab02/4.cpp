bdd2df2a9b7737c6b854524796a8aa34b9a5360d
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2024-05-06 14:02:38 +0545

    3rd commit

---
+++ lab02/4.cpp
@@ -0,0 +1,27 @@
+//intert 5 values in an array and display them using oop
+#include<iostream>
+using namespace std;
+class arre{
+    int arr[10];
+    int i;
+    public:
+        void getdata(){
+            for (i=0;i<10;i++){
+            cout<<"Enter term "<<i+1<<": ";
+            cin>>arr[i];
+            }
+        }
+        void display(){
+            for (i=0;i<10;i++){
+            cout<<arr[i];
+            }
+        }
+
+};
+int main(){
+    arre a1;
+    a1.getdata();
+    a1.display();
+    return 0;
+    
+}
